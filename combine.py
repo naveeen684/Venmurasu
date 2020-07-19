7e51c2df5e568e3fb8fc278d77493e98406f2794
Naveen <naveenrajapandian@gmail.com>
2020-07-19 19:20:00 +0530

    first commit

--- combine.py
+++ combine.py
@@ -3,7 +3,12 @@ import glob
 score = []
 
 # get all text files from data folder and combine it into single list
-for file in glob.glob("data/*.txt"):
+for file in glob.glob("data1/*.txt"):
+    print(file)
+    with open(file, 'r', encoding="utf-8") as f:
+        score = score+[line.rstrip('\n') for line in f]
+
+for file in glob.glob("data2/*.txt"):
     print(file)
     with open(file, 'r', encoding="utf-8") as f:
         score = score+[line.rstrip('\n') for line in f]
@@ -14,5 +19,6 @@ with open("combined/total.txt", "w", encoding="utf-8") as f:
         f.write(str(s) + "\n")
 
 
-print("Total no of blog entries: ", len(glob.glob("data/*.txt")))
+print("Total no of blog entries: ", len(
+    glob.glob("data1/*.txt"))+len(glob.glob("data2/*.txt")))
 print("Total no of words: ", len(score))
--- sort.py
+++ sort.py
@@ -1,6 +1,5 @@
 import numpy
 import operator
-import time
 import tamil
 
 
@@ -9,40 +8,45 @@ with open("combined/total.txt", "r", encoding="utf-8") as file:
 
 
 word_array = numpy.array(words)
+print('Count of the total Words found: ', len(word_array))
 
-start = time.time()
 
-word_array = sorted(word_array, reverse=True,
-                    key=lambda x: len(tamil.utf8.get_letters(x)))
-
-end = time.time()
-
-print(f"Runtime of the sorting is {end - start}")
-
-with open("result/sorted.txt", "w", encoding="utf-8") as f:
-    for s in word_array:
-        f.write(str(s) + " - "+str(len(tamil.utf8.get_letters(s))) + "\n")
-
-print('Find N Largest Words')
+print('Find N Most Occurring Words')
 n = int(input('Enter N:'))
-
-
-with open("result/Top10LongestWords.txt", "w", encoding="utf-8") as f:
-    i = 0
-    for s in word_array[:n]:
-        print(i, " "+str(s) + " - "+str(len(tamil.utf8.get_letters(s))))
-        f.write(str(s) + " - "+str(len(tamil.utf8.get_letters(s))) + "\n")
-        i += 1
-
-print('Find M Most Occurring Words')
-m = int(input('Enter M:'))
-frequency = {}
+unique_words = {}
 for word in word_array:
-    frequency[word] = 1 + frequency.get(word, 0)
+    unique_words[word] = 1 + unique_words.get(word, 0)
 
+print('Count of the unique Words found: ', len(unique_words))
 
-with open("result/Top10MostOccurringWords.txt", "w", encoding="utf-8") as f:
-    for l in sorted(frequency.items(), reverse=True, key=operator.itemgetter(1))[:m]:
+with open("result/Top%sMostOccurringWords.txt" % n, "w", encoding="utf-8") as f:
+    i = 1
+    for l in sorted(unique_words.items(), reverse=True, key=operator.itemgetter(1))[:n]:
         print(i, " "+str(l[0])+':'+str(l[1]))
         f.write(str(l[0])+':'+str(l[1]) + "\n")
         i += 1
+
+word_array = {k: v for k, v in sorted(unique_words.items(
+), reverse=True, key=lambda item: len(tamil.utf8.get_letters(item[0])))}
+
+with open("result/sorted.txt", "w", encoding="utf-8") as f:
+    for s in word_array.items():
+        f.write(str(s[0]) + "-" +
+                str(len(tamil.utf8.get_letters(s[0])))+"-"+str(s[1]) + "\n")
+
+
+print('Find M Longest Words')
+m = int(input('Enter M:'))
+
+
+with open("result/Top%sLongestWords.txt" % m, "w", encoding="utf-8") as f:
+    i = 1
+    for s in word_array.items():
+        if(i <= m):
+            print(i, " "+str(s[0]) + " - " +
+                  str(len(tamil.utf8.get_letters(s[0]))))
+            f.write(str(s[0]) + " - " +
+                    str(len(tamil.utf8.get_letters(s[0]))) + "\n")
+            i += 1
+        else:
+            break
